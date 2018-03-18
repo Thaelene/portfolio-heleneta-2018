915b2fb35c257765fdac296937adbae7594ba8cb
thaelene <helene.ta@hetic.net>
2018-03-18 23:45:36 +0100

    Initialize portfolio

---
+++ pages/contact.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="title">Salut</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Helene Ta Contact me'
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.title {
+  color: $test;
+  font-family: $avenir-black;
+  &:after {
+    content: 'caca';
+  }
+}
+</style>
